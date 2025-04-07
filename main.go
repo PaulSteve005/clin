14ece57d5de7c8fb890945bbee9310619d76589c
PaulSteve0005 <pranoypaulog@gmail.com>
2025-04-07 17:50:21 +0530

    workin for c somehow

--- main.go
+++ main.go
@@ -3,31 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
-func exec(extension string) {
+func do(extension string,pathToSource string) {
+	var cmd *exec.Cmd 
 	switch extension { 
-	case "c":
+	case ".c":
 		fmt.Println("gcc")
-	case "cpp":
+		cmd = exec.Command("gcc" ,pathToSource,"-o","/tmp/a.out")
+	case ".cpp":
 		fmt.Println("g++")
-	case "java":
+	case ".java":
 		fmt.Println("javac and java")
-	case "py":
+	case ".py":
 		fmt.Println("Python")
-	case "go":
+	case ".go":
 		fmt.Println("Golang")
-	case "js":
+	case ".js":
 		fmt.Println("node-js")
 	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("error during processing " , err )
+	}
+	run := exec.Command("/tmp/a.out")
+	run.Stdout = os.Stdout
+	run.Stderr = os.Stderr
+	run.Run()
 }
 
 func main()  {
 	//initial sanity check
 	if  len(os.Args) <   2{
 		fmt.Println("no source code specified")
+		return
 	}
-	filePath :=  os.Args[1]
-	fmt.Println("path entered ", filePath)
-	exec(filePath)
+	pathToSource := os.Args[1]
+	fmt.Println("path entered ", pathToSource)
 
+	// extension extraction
+	extension :=  filepath.Ext(pathToSource)
+	if extension != ""{
+		do(extension,pathToSource)
+	}else{
+		fmt.Println("no extension found ")
+	}
+	return
 }
