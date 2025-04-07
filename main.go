a47b97dc96842b82a8a227786600252d769f0ce7
PaulSteve0005 <pranoypaulog@gmail.com>
2025-04-07 16:27:29 +0530

    learned arguments and switchCase

--- main.go
+++ main.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"os"
 )
+func exec(extension string) {
+	switch extension { 
+	case "c":
+		fmt.Println("gcc")
+	case "cpp":
+		fmt.Println("g++")
+	case "java":
+		fmt.Println("javac and java")
+	case "py":
+		fmt.Println("Python")
+	case "go":
+		fmt.Println("Golang")
+	case "js":
+		fmt.Println("node-js")
+	}
+}
 
 func main()  {
 	//initial sanity check
 	if  len(os.Args) <   2{
 		fmt.Println("no source code specified")
 	}
-	var filePath string =  os.Args[1]
+	filePath :=  os.Args[1]
 	fmt.Println("path entered ", filePath)
+	exec(filePath)
+
 }
